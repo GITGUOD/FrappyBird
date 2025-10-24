f76a45cc82a1b0962fefc4f7f50752e8b6239d4e
Tonny Huynh <tonnyhuynh02@gmail.com>
2025-10-24 18:56:32 +0200

    Pipelines are now getting deleted

--- FlappyBird/Assets/MovingPipe.cs
+++ FlappyBird/Assets/MovingPipe.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class MovingPipe : MonoBehaviour
 {
     public float movement;
+    public float deathZone;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +13,11 @@ public class MovingPipe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += (Vector3.left * movement) * Time.deltaTime; // deltaTime gör så att framerate inte spelar någon roll
+
+        transform.position += Vector3.left * movement * Time.deltaTime; // deltaTime gör så att framerate inte spelar någon roll
+        if(transform.position.x < deathZone)
+        {
+            Destroy(gameObject);
+        }
     }
 }
