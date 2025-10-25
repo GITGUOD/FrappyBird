0ac750a4d0c6c0a9d6e5d293b71678f948fd658d
Tonny Huynh <tonnyhuynh02@gmail.com>
2025-10-25 23:39:50 +0200

    Flappybird completed, first phase finished

--- FlappyBird/Assets/BirdScript.cs
+++ FlappyBird/Assets/BirdScript.cs
@@ -5,19 +5,28 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
     public int jumpPower;
+    public LogicScript logicScript;
+    public bool birdAlive = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //gameObject.name = "KevinMcJ";   
+        logicScript = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && birdAlive)
         {
             myRigidBody.linearVelocity = Vector2.up * jumpPower;
 
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logicScript.gameOver();
+        birdAlive = false;
     }
 }
